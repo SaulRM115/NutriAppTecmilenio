29d5a74493eada0a01fb5b4debdeccbc6650ccd3
Saul Rivera <saul.rivera_mora@outlook.com>
2022-11-28 15:58:00 -0600

    Sigue sin funcionar el boton de mostrar resultados pero vamos bien y nada esta roto :)

--- app/src/main/java/com/example/tecmilenio_nutriapp/ui/EstimacionGET/GetFragment.java
+++ app/src/main/java/com/example/tecmilenio_nutriapp/ui/EstimacionGET/GetFragment.java
@@ -1,20 +1,32 @@
 package com.example.tecmilenio_nutriapp.ui.EstimacionGET;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.tecmilenio_nutriapp.R;
 import com.example.tecmilenio_nutriapp.databinding.FragmentEstimacionGetBinding;
+import com.example.tecmilenio_nutriapp.ui.AppStart.AppStartFragment;
 
 public class GetFragment extends Fragment {
 
+
+
     private FragmentEstimacionGetBinding binding;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +36,30 @@ public class GetFragment extends Fragment {
         binding = FragmentEstimacionGetBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        //BOTON
+        View viewC = inflater.inflate(R.layout.fragment_estimacion_get, container, false);
+        Button calcular = viewC.findViewById(R.id.calcular_variables_get);
+
+
+        //LinearLayout
+        View viewO = inflater.inflate(R.layout.fragment_estimacion_get, container, false);
+        LinearLayout observaciones = viewO.findViewById(R.id.observacion_container);
+
+        //Funcionalidad de BOTON
+
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AppStartFragment.class);
+                startActivity(intent);
+            }
+        });
+
         final TextView textView = binding.textGET; //
         getViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+
     }
 
     @Override
@@ -34,4 +67,7 @@ public class GetFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
+
 }

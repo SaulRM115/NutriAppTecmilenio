3c7c3ce2e96b17e8e04c442bd825db9d9206b339
Saul Rivera <saul.rivera_mora@outlook.com>
2022-12-01 17:16:47 -0600

    Se aniadio exitosamente la funcionalidad al boton de [Calcular Variables/Resultados]

--- app/src/main/java/com/example/tecmilenio_nutriapp/ui/EstimacionGET/GetFragment.java
+++ app/src/main/java/com/example/tecmilenio_nutriapp/ui/EstimacionGET/GetFragment.java
@@ -1,28 +1,26 @@
 package com.example.tecmilenio_nutriapp.ui.EstimacionGET;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.tecmilenio_nutriapp.R;
 import com.example.tecmilenio_nutriapp.databinding.FragmentEstimacionGetBinding;
-import com.example.tecmilenio_nutriapp.ui.AppStart.AppStartFragment;
 
 public class GetFragment extends Fragment {
 
 
+Button calculo;
 
     private FragmentEstimacionGetBinding binding;
 
@@ -30,36 +28,20 @@ public class GetFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GetViewModel getViewModel;
-        getViewModel = new ViewModelProvider(this).get(GetViewModel.class);
 
-        binding = FragmentEstimacionGetBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        //BOTON
-        View viewC = inflater.inflate(R.layout.fragment_estimacion_get, container, false);
-        Button calcular = viewC.findViewById(R.id.calcular_variables_get);
-
-
-        //LinearLayout
-        View viewO = inflater.inflate(R.layout.fragment_estimacion_get, container, false);
-        LinearLayout observaciones = viewO.findViewById(R.id.observacion_container);
-
-        //Funcionalidad de BOTON
-
-        calcular.setOnClickListener(new View.OnClickListener() {
+        View v = inflater.inflate(R.layout.fragment_estimacion_get, container, false);
+        calculo = v.findViewById(R.id.calcular_variables_get);
+        calculo.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AppStartFragment.class);
-                startActivity(intent);
+                Toast.makeText(getContext(), "Cosas", Toast.LENGTH_SHORT).show();
             }
+
         });
 
-        final TextView textView = binding.textGET; //
-        getViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+       return v;
 
-
+       
     }
 
     @Override
@@ -67,7 +49,5 @@ public class GetFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
-
 
 }
